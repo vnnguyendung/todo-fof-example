cecbfecee144998a5f4a1a604134f305083068dd
Nicholas Dionysopoulos <nicholas@akeebabackup.com>
2012-04-29 14:06:16 +0300

    Autofilter front-end items

---
+++ component/frontend/controllers/items.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * @package todo
+ * @copyright Copyright (c)2012 Nicholas K. Dionysopoulos / AkeebaBackup.com
+ * @license GNU General Public License version 3 or later
+ */
+
+defined('_JEXEC') or die();
+
+class TodoControllerItems extends FOFController
+{
+	/**
+	 * This runs before the browse() method. Return false to prevent executing
+	 * the method.
+	 * 
+	 * @return bool
+	 */
+	public function onBeforeBrowse() {
+		$result = parent::onBeforeBrowse();
+		if($result) {
+			// Get the current order by column
+			$orderby = $this->getThisModel()->getState('filter_order','');
+			// If it's not one of the allowed columns, force it to be the "ordering" column
+			if(!in_array($orderby, array('todo_item_id','ordering','title','due'))) {
+				$orderby = 'ordering';
+			}
+			// Apply ordering and filter only the enabled items
+			$this->getThisModel()
+				->filter_order($orderby)
+				->enabled(1);
+		}
+		return $result;
+	}
+}
