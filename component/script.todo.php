d00dede9f57e5af0fe568c32c11b37a52c45f7c3
Nicholas Dionysopoulos <nicholas@akeebabackup.com>
2013-02-21 17:44:06 +0200

    Changing license to GPLv2 or later to match the new FOF license and help developers who want to use GPLv2 for any reason.

--- component/script.todo.php
+++ component/script.todo.php
@@ -2,7 +2,7 @@
 /*
  * @package todo
  * @copyright Copyright (c)2012 Nicholas K. Dionysopoulos / AkeebaBackup.com
- * @license GNU General Public License version 3 or later
+ * @license GNU General Public License version 2 or later
  */
 defined('_JEXEC') or die();
 
