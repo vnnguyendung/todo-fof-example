da417a7a3d0e4fd7b75a55036ead532fc7a41367
George Wilson <georgejameswilson@googlemail.com>
2013-07-28 10:04:48 +0100

    Add return if FOF isn't installed

--- component/backend/todo.php
+++ component/backend/todo.php
@@ -11,6 +11,8 @@ defined('_JEXEC') or die();
 include_once JPATH_LIBRARIES.'/fof/include.php';
 if(!defined('FOF_INCLUDED')) {
 	JError::raiseError ('500', 'FOF is not installed');
+	
+	return;
 }
 
 FOFDispatcher::getTmpInstance('com_todo')->dispatch();
