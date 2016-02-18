0d7c0c0670d5288e848e01dc337ba3544a21a26a
willfaulds <willfaulds@gmail.com>
2016-02-18 11:15:25 +0000

    update Model/Item.php
    example of FOF3 trigger event to edit form field data with
    ‘getFieldnameAttribute’

--- component/backend/Model/Item.php
+++ component/backend/Model/Item.php
@@ -1,8 +1,33 @@
 <?php
+/**
+ * @package   To-do
+ * @copyright Copyright (c)2013-2015 Nicholas K. Dionysopoulos / AkeebaBackup.com (addition from FOF3 community)
+ * @license   GNU General Public License version 2 or later
+ */
+ 
+ /**
+ ===DOCUMENTAION=== 
+ https://github.com/akeeba/fof/wiki/The-Model
+ https://github.com/akeeba/fof/wiki/The-DataModel
+ 
+ https://github.com/akeeba/fof/wiki/Using-Joomla%21-plugins-to-handle-FOF-events
+ 
+ Tip: You can see all the events called by a FOF class, e.g. FOF30\Model\DataModel,
+ by opening its code and searching for triggerEvent.
+ Remember to also check its parent class, e.g. FOF30\Model\Model,
+ in case some of the events are defined there.
+ */
 
 namespace Akeeba\Todo\Admin\Model;
 
 class Item extends \FOF30\Model\DataModel
 {
     // Model implementation goes here
+    // i.e. getFieldnameAttribute
+    
+    // example for com_todo getTitleAttribute (title is a field used by the Item view)
+	protected function getTitleAttribute($value)
+	{
+		return $value;
+	}
 }
